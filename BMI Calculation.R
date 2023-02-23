e5267ac6a9c587dc884dd3fe98cfe72982437267
Md Abdur Rahman <85950644+sumon18145@users.noreply.github.com>
2023-02-23 14:55:19 +0600

    Update BMI Calculation.R

--- BMI Calculation.R
+++ BMI Calculation.R
@@ -1,16 +1,22 @@
+# Load the relevant library
 library(dplyr)
 library(tidyr)
+
+# Load the data sets
 data("starwars")
 
+# Cleaning and Managing the data set
 starwars_clean <- starwars %>% 
   select(gender, mass, height,species) %>% 
   filter(species == "Human") %>% 
   na.omit() %>% 
   mutate(height = height/100)
 
+# Calculating BMI
 BMI <- starwars_clean %>% 
   mutate(BMI = mass/height^2)
 
+# Calculating Average BMI
 Average_BMI <- BMI %>% 
   group_by(gender) %>% 
   summarise(Average_BMI = mean(BMI))
